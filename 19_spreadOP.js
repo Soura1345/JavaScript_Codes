7632d1b2b8127f261532b008362b4a22536be5cf
Sourashis Pal <palsourashis04@gmail.com>
2025-07-27 12:39:22 +0530

    add

---
+++ 19_spreadOP.js
@@ -0,0 +1,20 @@
+/* ... is the spread operator which allows an array or 
+string to be expanded into seperate element */
+
+// let number = [1, 2, 3, 4, 5];
+
+// let maximun = Math.max(...number);
+// let minimum = Math.min(...number);
+
+// console.log(maximun);
+// console.log(minimum);
+
+// let username = "Sourashis Pal";
+// let letters = [...username];
+
+// console.log(letters.join(''));
+
+let fruit = ["apple","orange", "banana"]
+let newFruit = [...fruit];
+
+console.log(newFruit)
