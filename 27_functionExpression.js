458348bb7153a31bd7a0168b3eb4a0bc3382a9e9
Sourashis Pal <palsourashis04@gmail.com>
2025-08-11 22:32:43 +0530

    function expression

---
+++ 27_functionExpression.js
@@ -0,0 +1,46 @@
+// setTimeout(function(){
+//     console.log("hello");
+// }, 300);
+
+const numbers = [1, 2, 3, 4, 5, 6]
+
+const square = numbers.map(
+    function(element){
+        return Math.pow(element, 2);
+    }
+);
+
+console.log(square);
+
+const cube = numbers.map(
+    function(numbers){
+        return Math.pow(numbers,3);
+    }
+);
+
+console.log(cube);
+
+const evenNum = numbers.filter(
+    function(element){
+        return element % 2 === 0;
+    }
+);
+
+console.log(evenNum);
+
+const oddNum = numbers.filter(
+    function(element){
+        return element % 2 !== 0;
+    }
+);
+
+console.log(oddNum);
+
+const addNum = numbers.reduce(
+    function(accumulator,element){
+        return accumulator + element;
+    }
+);
+
+console.log(addNum);
+
