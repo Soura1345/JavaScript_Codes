af0cc48c5c5b32d63ce0234ff33a6397a09edc00
Sourashis Pal <palsourashis04@gmail.com>
2025-08-14 12:22:08 +0530

    super

---
+++ 35_super.js
@@ -0,0 +1,66 @@
+class Animal{
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+
+    move(speed){
+        console.log(`This ${this.name} moves at a speed of ${speed} kpm`)
+    }
+}
+
+class Rabbit extends Animal{
+    constructor(name, age, runSpeed){
+        super(name, age);
+        // this.runSpeed = runSpeed;
+    }
+
+    run(){
+        console.log(`This ${this.name} can run.`);
+        super.move(this.runSpeed);
+    }
+}
+
+class Fish extends Animal{
+    constructor(name, age, swimSpeed){
+        super(name, age);
+        // this.swimSpeed = swimSpeed;
+    }
+
+    swim(){
+        console.log(`This ${this.name} can swim.`);
+        super.move(this.swimSpeed);
+    }
+}
+
+class Hawk extends Animal{
+    constructor(name, age, flySpeed){
+        super(name, age);
+        // this.flySpeed = flySpeed;
+    }
+
+    fly(){
+        console.log(`This ${this.name} can fly`);
+        super.move(this.flySpeed);
+    }
+}
+
+const rabbit = new Rabbit("rabbit", 1, 10);
+const fish = new Fish("fish", 2, 24);
+const hawk = new Hawk("hawk", 3, 30);
+
+// console.log(rabbit.name);
+// console.log(rabbit.age);
+// console.log(rabbit.runSpeed);
+
+// console.log(fish.name);
+// console.log(fish.age);
+// console.log(fish.swimSpeed);
+
+// console.log(hawk.name);
+// console.log(hawk.age);
+// console.log(hawk.flySpeed);
+
+rabbit.run();
+fish.swim();
+hawk.fly();
